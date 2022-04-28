18312555059e6d870aad6aded86b9e16bec1a69c
Ayakanoji <104145422+Ayanok0ji@users.noreply.github.com>
2022-04-28 20:31:06 +0800

    Update and rename MYAFS.lua to SCRIPTS.lua

--- MYAFS.lua
+++
@@ -1,5 +0,0 @@
--- Made by ayanakoji#9273
--- It only works on Dimension 2 and 3
--- Get key here: https://raw.githubusercontent.com/Ayanok0ji/RVXLPOIT-SCRIPTS/main/KEYS.md
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Ayanok0ji/RVXLPOIT-SCRIPTS/main/AFS', true))()
---
+++ SCRIPTS.lua
@@ -0,0 +1,4 @@
+local id = game.PlaceId
+if id == 537413528 --BAB
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/KiyotakaAyakanoji/SCRIPTS/main/BAB")()
+end
